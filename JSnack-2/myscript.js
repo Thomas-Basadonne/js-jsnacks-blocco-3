ca68d1b47728f3238f1d6c8fa09291fd8880d70d
Thomas-Basadonne <thommybasa@gmail.com>
2023-01-30 17:13:48 +0100

    commit

--- JSnack-2/myscript.js
+++ JSnack-2/myscript.js
@@ -1,2 +1,14 @@
 // Snack2
 // Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+let myArray = [];
+
+let sum = 0;
+
+while (sum < 50) {
+  let newNumber = parseInt(prompt("scegli un numero minore di 50"));
+  myArray.push(newNumber);
+  sum += myArray;
+}
+
+console.log(myArray);
